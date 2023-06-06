b83703bdb1f9840aa9cb783739f25f9d29a911d7
Sebastian Nowak <nowaksebastian998@gmail.com>
2023-06-06 21:53:06 +0200

    first commit

---
+++ src/main/java/air/nowak/sebastian/Expense.java
@@ -0,0 +1,2 @@
+package air.nowak.sebastian;public class Expense {
+}
---
+++ src/main/java/air/nowak/sebastian/Main.java
@@ -0,0 +1,5 @@
+package air.nowak.sebastian;public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
